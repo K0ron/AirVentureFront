5c658453d48f2265258b85f7ddb44c4ddbeb1a7b
Caron Kevin <keks@Host-001.lan>
2025-01-03 19:01:55 +0100

    add search by categories (not fonctional)

---
+++ src/app/Home/ui/component/search-categories/search-categories.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-search-categories',
+  standalone: true,
+  imports: [],
+  templateUrl: './search-categories.component.html',
+  styleUrl: './search-categories.component.scss'
+})
+export class SearchCategoriesComponent {
+
+}
