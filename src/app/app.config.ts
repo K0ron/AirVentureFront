1f5475f8d60d70b6ce0691a48f391041ad2cae5b
K0ron <kevin.caron.91@gmail.com>
2025-01-22 18:40:49 +0100

    UI login modal

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -12,8 +12,6 @@ import {
 import { CredentialsInterceptor } from './core/interceptors/credentials.interceptor';
 import { AuthInterceptor } from './core/interceptors/auth.interceptor';
 import { CookieAuthInterceptor } from './core/interceptors/cookie-auth.interceptor';
-import { providePrimeNG } from 'primeng/config';
-import Aura from '@primeng/themes/aura';
 
 export const appConfig: ApplicationConfig = {
   providers: [
@@ -28,8 +26,6 @@ export const appConfig: ApplicationConfig = {
         headerName: 'X-XSRF-TOKEN',
       })
     ),
-    provideAnimationsAsync(),
-
     {
       provide: HTTP_INTERCEPTORS,
       useClass: CredentialsInterceptor,
