cfe6f2598a74b9ea78999b0d2c51f49ad68d0688
K0ron <kevin.caron.91@gmail.com>
2025-02-12 10:39:26 +0100

    search bar stand-by

---
+++ src/app/Header/component/search-bar/search-bar.component.ts
@@ -0,0 +1,43 @@
+import { Component, Output, EventEmitter } from '@angular/core';
+import { DividerModule } from 'primeng/divider';
+import { FormsModule } from '@angular/forms';
+import { SearchService } from '../../domain/services/search.service';
+import { debounceTime, distinctUntilChanged, Subject } from 'rxjs';
+
+@Component({
+  selector: 'app-search-bar',
+  standalone: true,
+  imports: [DividerModule, FormsModule],
+  templateUrl: './search-bar.component.html',
+  styleUrl: './search-bar.component.scss',
+})
+export class SearchBarComponent {
+  public enteredActivityNameValue: string = '';
+  public enteredCityValue: string = '';
+  private searchNameSubject = new Subject<string>();
+  private searchCitySubject = new Subject<string>();
+
+  constructor(private searchService: SearchService) {
+    this.searchNameSubject
+      .pipe(debounceTime(300), distinctUntilChanged())
+      .subscribe((searchQuery) => {
+        this.searchService.searchName(searchQuery);
+        console.log('ACTIVITY SEARCH', searchQuery);
+      });
+
+    this.searchCitySubject
+      .pipe(debounceTime(300), distinctUntilChanged())
+      .subscribe((searchQuery) => {
+        this.searchService.searchCity(searchQuery);
+        console.log('ACTIVITY SEARCH CITY', searchQuery);
+      });
+  }
+
+  onSearchActivityByName(): void {
+    this.searchNameSubject.next(this.enteredActivityNameValue);
+  }
+
+  onSearchActivityByCity(): void {
+    this.searchCitySubject.next(this.enteredCityValue);
+  }
+}
