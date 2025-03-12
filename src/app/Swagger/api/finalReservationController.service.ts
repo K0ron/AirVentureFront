8146053fbea3032d8c4e5ed7e7d9f35af36f7be5
K0ron <kevin.caron.91@gmail.com>
2025-03-12 20:01:29 +0100

    cahnge baseurl swagger

--- src/app/Swagger/api/finalReservationController.service.ts
+++ src/app/Swagger/api/finalReservationController.service.ts
@@ -20,10 +20,11 @@ import { FinalReservation } from '../models/finalReservation';
 
 import { BASE_PATH, COLLECTION_FORMATS } from '../configurations/variables';
 import { Configuration } from '../configurations/configuration';
+import { environment } from '../../../../environments/environment';
 
 @Injectable()
 export class FinalReservationControllerService {
-  protected basePath = 'https://localhost:8080';
+  protected basePath = environment.apiBaseUrl;
   public defaultHeaders = new HttpHeaders();
   public configuration = new Configuration();
 
--- src/app/Swagger/api/reviewController.service.ts
+++ src/app/Swagger/api/reviewController.service.ts
@@ -20,10 +20,11 @@ import { Review } from '../models/review';
 
 import { BASE_PATH, COLLECTION_FORMATS } from '../configurations/variables';
 import { Configuration } from '../configurations/configuration';
+import { environment } from '../../../../environments/environment';
 
 @Injectable()
 export class ReviewControllerService {
-  protected basePath = 'https://localhost:8080';
+  protected basePath = environment.apiBaseUrl;
   public defaultHeaders = new HttpHeaders();
   public configuration = new Configuration();
 
